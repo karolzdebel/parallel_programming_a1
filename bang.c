27dcd03f6b227f051925acd4bc062a978dd64f76
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-10-02 16:29:20 -0400

    Added data structures and functions responsible for filling them from the data file.

--- bang.c
+++ bang.c
@@ -1,11 +1,107 @@
 #include "bang.h"
 
 int main(int argc,char **argv){
+	FILE *file;
 
 	if (argc < 2){
 		printf("Please provide csv file as argument.\n");
 		return(EXIT_FAILURE);
 	}
-	printf("%s\n",argv[1]);
+
+	file = fopen(argv[1],"r");
+	getDataset(file,SIZE_HEADER,SIZE_RECORD*100);
 	return 0;
+}
+Record *getRecord(char *line){
+	return NULL;
+}
+
+bool sameCol(Record rec1, Record rec2){
+	return false;
+}
+
+static int recCount(Dataset *dataset){
+	return dataset->recNum;
+}
+
+static int colCount(Dataset *dataset){
+	return dataset->colNum;
+}
+
+
+static void addRecord(Dataset *dataset, Record *record){
+	Record **arr = dataset->records;
+
+	dataset->recNum++;
+	
+	/*Allocate more memory for new record*/
+	if (arr != NULL){
+		arr = realloc(arr,sizeof(Record*)*recNum(dataset));
+		arr[recNum-1] = record;
+	}
+	else{
+		arr = malloc(sizeof(Record*));
+		arr[0] = record;
+	}
+}
+
+static void addIndex(Dataset *dataset, int index){
+	int *arr = dataset->collisionIndex;
+
+	if (arr != NULL){
+		arr = realloc(arr,sizeof(int)*colCount(dataset));
+		arr[colNum-1] = index;
+	}
+	else{
+		arr = malloc(sizeof(int));
+		arr[0] = index;
+	}
+}
+
+static void createDataset(Dataset **dataset){
+	*dataset = malloc(sizeof(Dataset));
+	(*dataset)->colNum = 0;
+	(*dataset)->recNum = 0;
+	(*dataset)->collisionIndex = NULL;
+	(*dataset)->records = NULL; 
+}
+
+Dataset *getDataset(FILE *file, int startPos, int length){
+	Dataset *dataset = NULL;
+	Record *prevRecord = NULL;
+	Record *curRecord = NULL;
+	char line[SIZE_RECORD+1];
+	int readCount = length/SIZE_RECORD;		//Calculate neccessary amount of reads
+
+	createDataset(&dataset);
+
+	/*Go to provided address in file*/
+	fseek(file,startPos,0);
+
+	/*Read line from data file provided*/
+	for(int i=0; i<readCount; i++){
+
+		fread((void*)line,sizeof(char),SIZE_RECORD+1,file);
+		printf("%s",line);
+		/*Add record to dataset*/
+		addRec(dataset);
+
+		/*Store previous record for later comparison*/
+		prevRecord = curRecord;
+		curRecord = getRecord(line);
+
+		/*Don't compare if the first record is being parsed*/
+		if (prevRecord != NULL){
+			
+			/*Compare record with previous to see if it is part of the same collision*/
+			if (!sameCol(*prevRecord,*curRecord)){
+
+				/*If not part of the same collision than create new index*/
+				dataset->colNum++;
+				addIndex(dataset, colNum(dataset), recNum(dataset));
+			}
+		}
+	}  
+
+	return dataset;
 }
--- bang.h
+++ bang.h
@@ -1,2 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define SIZE_RECORD 61
+#define SIZE_HEADER 145
+
+typedef struct Date Date;
+typedef struct Date {
+	int year;			//Year collision occured
+	int month;			//Month collision occured ranging 1-12, -1 for unspecified
+	int day;			//Day collision occured ranging 1-7, -1 for unspecified
+} Date;
+
+typedef struct Record Record;
+typedef struct Record {
+	Date date;			//Date the collision occured
+	char *location;		//Code of accident location 1-12 and QQ
+	char *gender;		//Gender of individual involved
+	int vehYear;		//Vehicle year involved in collision
+	bool death;			//Whether the person involved in the collision died
+} Record;
+
+typedef struct Dataset Dataset;
+typedef struct Dataset {
+	int colNum;				//Number of collisions found in dataset
+	int recNum;				//Number of records found in dataset
+	int *collisionIndex;	//Array storing indexes of each collsion
+	Record **records;		//Array of records addresses
+} Dataset;
+
+
+/*********************************************************************
+ * FUNCTION NAME: getDataset
+ * PURPOSE: Stores record data and collision indexes.
+ * ARGUMENTS: . File to be read from.
+ *		      . Position of the file to start reading from.
+ *			  . Length of reading portion.
+ * RETURNS: Address of Dataset containing all records found in the 
+ *			portion provided.
+ *********************************************************************/
+Dataset *getDataset(FILE *file, int startPos, int length);
+
+/*********************************************************************
+ * FUNCTION NAME: getRecord
+ * PURPOSE: Stores record data from a line retrieved from data file.
+ * ARGUMENTS: . Line of text from data file.
+ * RETURNS: Record containing parsed data.
+ *********************************************************************/
+Record *getRecord(char *line);
+
+/*********************************************************************
+ * FUNCTION NAME: sameCol
+ * PURPOSE: Checks if two records are part of the same collion.
+ * ARGUMENTS: . Record being compared.
+ *			  . Record being compared.
+ * RETURNS: True if the records are part of the same collision,
+ *		    false otherwise.
+ *********************************************************************/
+bool sameCol(Record rec1, Record rec2);
+
