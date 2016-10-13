81201990adb5cd37beed12b9dedc4b210f4cb31c
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-10-12 20:01:02 -0400

    Final files

---
+++ bang.h
@@ -0,0 +1,5 @@
+Name: Karol Zdebel
+Student Number: 0892519
+File Name: bang.c
+
+Purpose: Store collision data and process requested queries by distributing the work amongst the available processors.
